f87c0ba9a18c4a4573228eb90e6f8ab0177e6657
Vicky Ahuja <vickyahuja371@gmail.com>
2022-12-17 14:54:00 +0530

    added animated splash to RN ios App

---
+++ ios/AnimatedSplashDemo-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+//  AnimatedSplashDemo-Bridging-Header
+
+#ifndef AnimatedSplashDemo_Bridging_Header_h
+#define AnimatedSplashDemo_Bridging_Header_h
+
+#import "RNSplashScreen.h" // here
+
+#endif /* AnimatedSplashDemo_Bridging_Header_h */
---
+++ ios/LogoAnimationView.swift
@@ -0,0 +1,30 @@
+//
+//  LogoAnimationView.swift
+//  AnimatedSplashDemo
+//
+//  Created by neosoft on 16/12/22.
+//
+
+import Foundation
+
+import UIKit
+import Lottie
+
+@objc class LogoAnimationView: NSObject {
+
+  @objc func createAnimationView(rootView: UIView, lottieName: String) -> AnimationView {
+    let animationView = AnimationView(name: lottieName)
+    animationView.frame = rootView.frame
+    animationView.center = rootView.center
+    animationView.backgroundColor = UIColor.white;
+    return animationView;
+  }
+
+  @objc func play(animationView: AnimationView) {
+    animationView.play(
+      completion: { (success) in
+        RNSplashScreen.setAnimationFinished(true)
+      }
+    );
+  }
+}
